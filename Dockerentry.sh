eaea228da90c77aa8964a79e4cd3994340dade42
lucdev <me@lucdev.net>
2023-01-24 16:59:19 -0300

    Dockerentry now restores ssh config

--- Dockerentry.sh
+++ Dockerentry.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+if [ -d "$HOME/.ssh" ]; then
+    rm -rf "$HOME/.ssh"
+fi
+
+if [ ! -z "${SSH_KEY_PUB}" ] && [ ! -z "${SSH_KEY_PRIV}" ] && [ ! -z "${SSH_KNOW_HOSTS}" ]; then
+    mkdir -vp $HOME/.ssh
+    echo $SSH_KEY_PUB | base64 -d > $HOME/.ssh/id_ed25519.pub
+    echo $SSH_KEY_PRIV | base64 -d > $HOME/.ssh/id_ed25519
+    echo $SSH_KNOW_HOSTS | base64 -d > $HOME/.ssh/known_hosts
+
+    chmod -v 700 $HOME/.ssh
+    chmod -v 600 $HOME/.ssh/id_ed25519
+    chmod -v 644 $HOME/.ssh/id_ed25519.pub
+    chmod -v 644 $HOME/.ssh/known_hosts
+fi
+
 if [ "${ENV}" == 'production' ]; then
-     python -m gunicorn --chdir src main:app -w $GUNICORN_WORKERS --threads $GUNICORN_THREADS -b $HOST:$PORT
+    python -m gunicorn --chdir src main:app -w $GUNICORN_WORKERS --threads $GUNICORN_THREADS -b $HOST:$PORT
 else
     cd src && python main.py
 fi
