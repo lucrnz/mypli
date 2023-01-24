eac8374fda8af1856a6c020c9bdcb4c1df51f73b
lucdev <me@lucdev.net>
2023-01-24 17:01:08 -0300

    The api is now able to pull changes from an specific repo

--- src/main.py
+++ src/main.py
@@ -1,13 +1,43 @@
-from os import getenv
-from flask import Flask
-
+from os import getenv, environ
+from os.path import exists
+from flask import Flask, jsonify
+import subprocess
 app = Flask(__name__)
 
 app.config['DEBUG'] = (getenv('DEBUG') or '0').lower() in ['1', 'true']
 
+srv_path = getenv('SRV_PATH') or ''
+
+if not exists(srv_path):
+    raise Exception(
+        'SRV_PATH is not a valid path. Please check the environment file.')
+
+safe_env = environ.copy()
+
+for entry in ['SECRET_KEY', 'SSH_KEY_PRIV', 'SSH_KEY_PUB',
+              'SSH_KNOW_HOSTS', 'DEBUG', 'HOST', 'PORT', 'USER_ID', 'GROUP_ID']:
+    safe_env.pop(entry, None)
+
+
 @app.route('/')
 def index():
-    return 'Hello world!'
+    meme_url = "https://images7.memedroid.com/images/UPLOADED501/57f66e907bb62.jpeg"
+    return f'<html><body><img src="{meme_url}" style="max-width: 100%"/></body></html>'
+
+
+@app.route('/deploy/lucsite')
+def deploy_lucsite():
+    cmd = subprocess.run(["git", "pull", "origin", "main"], universal_newlines=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=f"{srv_path}/lucsite", env=safe_env)
+    res = {}
+    res["returncode"] = cmd.returncode
+    if len(cmd.stdout) > 0:
+        res["stdout"] = cmd.stdout
+
+    if len(cmd.stderr) > 0:
+        res["stderr"] = cmd.stderr
+    return jsonify(res)
+
 
 if __name__ == '__main__':
     app.run(host=getenv('HOST') or '::', port=getenv('PORT') or '8080')
