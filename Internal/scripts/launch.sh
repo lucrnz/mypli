405aa689a69162be0028d47d8c643080191b35d8
lucdev <me@lucdev.net>
2023-01-24 22:42:14 -0300

    Internal- Add launch script

--- Internal/scripts/launch.sh
+++ Internal/scripts/launch.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Warning: use this script from the "Internal" directory, not the ./scripts directory!
 
+if [ ! -f ".env" ]; then
+    export ENV="production"
+else
+    export ENV=$(grep ENV= .env)
+fi
+
 if [ "${ENV}" == 'production' ]; then
     python -m gunicorn --chdir src main:app -w $GUNICORN_WORKERS --threads $GUNICORN_THREADS -b $HOST:$PORT
 else
