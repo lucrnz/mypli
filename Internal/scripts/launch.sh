3efee70d93e315271fbe211c327b4811bdd12156
lucdev <me@lucdev.net>
2023-01-26 15:14:35 -0300

    Add venv option for launch script

--- Internal/scripts/launch.sh
+++ Internal/scripts/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Warning: use this script from the "Internal" directory, not the ./scripts directory!
 
+if [ ! -z "$1" ] && [ "$1" == "-venv" ]; then
+    export PATH="$(pwd)/.venv/bin:$PATH"
+fi
+
 if [ ! -f ".env" ]; then
     export ENV="production"
 else
