f199a01a1dc64ca1e36ad002bfee5f0f50dd3f3c
lucdev <me@lucdev.net>
2023-01-30 21:57:20 -0300

    Remove python code

--- src/remote_bash.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-
-def remote_bash(host_name: str, bash_code: str):
-    return subprocess.run(['ssh', host_name, 'bash'], universal_newlines=True,
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=bash_code)
--- src/wrap_bash_code_service.py
+++
@@ -1,2 +0,0 @@
-def wrap_bash_code_service(bash_code: str, host_dir: str, service_name: str) -> str:
-    return f'cd {host_dir}/{service_name} || exit $?\n{bash_code}'
